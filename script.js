a81575ea8c312c8ebb16c08c8b3e43edc2e46b1e
samarpan <samarpankarra@gmail.com>
2021-09-04 16:19:23 +0530

    event

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ function openTab(event , tabName){
     tabcontents.forEach((event1) => (event1.style.display = "none"));
     tabLinks.forEach((tabLink) => tabLink.classList.remove("active"));
     event.currentTarget.classList.add("active");
-    document.getElementById(tabName).style.display="block";
+    document.getElementById(tabName).style.display="flex";
 }
 
 
@@ -36,7 +36,7 @@ function openCity(evt, cityName) {
     // Get all elements with class="tablinks" and remove the class "active"
     tablinks = document.getElementsByClassName("tablinks");
     for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" active", "");
+        tablinks[i].className = tablinks[i].className.replace("active","");
     }
 
     // Show the current tab, and add an "active" class to the button that opened the tab
